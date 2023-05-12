00190921d138737380681cbab2bd4996bf802538
Edisson <124311704+Edissonly@users.noreply.github.com>
2023-05-11 19:49:41 -0500

    yield

---
+++ M3AGIT/src/m3agit/Proceso2.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m3agit;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Joseline
+ */
+
+public class Proceso2 extends Thread{
+String[] nom={"Buenos dias","Buenos tardes","Buenos noches"};
+private JLabel nombre;
+public Proceso2(JLabel nombre){
+    this.nombre = nombre;
+}
+    @Override
+    public void run(){
+        int i=0;
+        while(true){
+            this.nombre.setText(nom[i]+" "+i);
+            //System.out.println(nom[i]);
+            i++;
+            if(i==nom.length){
+                i=0;         
+            }
+            try {
+                sleep(1000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Proceso2.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
---
+++ M3AGIT/src/m3agit/Proceso3.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m3agit;
+
+import static java.lang.Thread.sleep;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Joseline
+ */
+public class Proceso3 extends Thread {
+String[] nom={"Desayuno","Almuerzo","Cena"};
+private JLabel nombre;
+public Proceso3(JLabel nombre){
+    this.nombre = nombre;
+}
+    public void run(){
+        int i=0;
+        while(true){
+            this.nombre.setText(nom[i]+" "+i);
+            //System.out.println(nom[i]+" "+i);
+            i++;
+            if(i==nom.length){
+                i=0;
+                Thread.yield();
+                System.out.println("entre al yield");
+            }
+            try {
+                sleep(1000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Proceso2.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
